c24d86e7f9d7c966c9b6051f8ac477064ce938a0
ritesh_gatane <riteshgatane2006@gmail.com>
2026-07-15 01:44:39 +0530

    Add files via upload

--- Assignment_10/program10_1.py
+++ Assignment_10/program10_1.py
@@ -1,1 +1,18 @@
+def Multiplication(No):
 
+    for i in range(1,11):
+        Mult = (No * i)
+        print(Mult)
+        
+    
+
+
+def main():
+    Value = int(input("Enter the NUmber :"))
+
+    Ret = Multiplication(Value)
+
+
+
+if  __name__ =="__main__":
+    main()
