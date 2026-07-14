fa06b8a44390e200fe8dff5983c3a9b72e45b88a
ritesh_gatane <riteshgatane2006@gmail.com>
2026-07-15 01:46:58 +0530

    Add files via upload

---
+++ Assignment_10/program10_5.py
@@ -0,0 +1,18 @@
+def Odd(No):
+    
+    for i in range(1,No+1,2):
+        print(i)
+        
+
+
+def main():
+    Value = int(input("Enter the NUmber :"))
+
+    Odd(Value)
+
+    
+
+
+
+if  __name__ =="__main__":
+    main()
