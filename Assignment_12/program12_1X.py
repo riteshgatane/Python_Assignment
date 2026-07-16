c09cc7c213d5abd7deed1fef722a0b404a1bb36a
ritesh_gatane <riteshgatane2006@gmail.com>
2026-07-17 00:08:35 +0530

    Add files via upload

---
+++ Assignment_12/program12_1X.py
@@ -0,0 +1,13 @@
+#Check Vowel 
+
+char =(input("Enter the Charater to check the it is Vowel or Not: "))
+
+char = char.lower()
+
+if(char == 'a' or char == 'e' or char == 'i' or char =='o' or char == 'u'):
+    print(f"{char} is a Vowel")
+
+else:
+    {
+        print(f"{char} is not a Vowel ")
+    }
