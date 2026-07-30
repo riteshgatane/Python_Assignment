7f61e7220a9e44c2950acfc1e4e13e8c7d859cb1
ritesh_gatane <riteshgatane2006@gmail.com>
2026-07-30 13:43:23 +0530

    Add files via upload

---
+++ Assignment_12/program12_4.py
@@ -0,0 +1,17 @@
+def Display(Value):
+    i = 0 
+    for i in range(1 ,Value+1):
+        print(i)
+    
+
+def main():
+    iRet = 0 
+    A = int(input("Enter the First NUmber :"))
+
+    Display(A)
+    
+    
+
+
+if __name__ == "__main__":
+    main()
