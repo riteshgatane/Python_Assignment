d144d6aadc1c563ff2a42e87f279109a41e9424e
ritesh_gatane <riteshgatane2006@gmail.com>
2026-08-01 05:58:45 +0530

    Add files via upload

---
+++ Assignment_14/program14_7.py
@@ -0,0 +1,18 @@
+def Divisibleby5( No ):
+    if(No % 5 == 0):
+        return 1
+    else:
+        return 0
+
+def main():
+    Value1 = int(input("Enter the Numbers:"))
+
+    Ret = Divisibleby5(Value1)
+
+    if(Ret == 1 ):
+        print(f"It is a divisible by 5")
+    else:
+        print("It is Not a divisible by 5 ")
+
+if __name__ == "__main__":
+    main()
