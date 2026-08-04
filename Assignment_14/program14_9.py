0988997f167d22511e061c83a1f7af42954f3839
ritesh_gatane <riteshgatane2006@gmail.com>
2026-08-04 07:47:26 +0530

    Add files via upload

---
+++ Assignment_14/program14_9.py
@@ -0,0 +1,12 @@
+Multiplication = lambda No1 ,No2 : No1*No2  
+
+def main():
+    Value1 = int(input("Enter the  First Numbers:"))
+    Value2 = int(input("Enter the  Second  Numbers:"))
+
+    Ret = Multiplication(Value1,Value2)
+    print(f"Addition of {Value1} and {Value2} : {Ret}")
+
+
+if __name__ == "__main__":
+    main() 
