433bf28a4265c459a936a1b2af44d726659d9560
ritesh_gatane <riteshgatane2006@gmail.com>
2026-08-06 17:52:32 +0530

    Add files via upload

---
+++ Assignment_28/program3.py
@@ -0,0 +1,18 @@
+import os 
+
+def Display(FileName):
+    fobj = open(FileName, "r")
+
+    Line  = fobj.readlines()
+
+    for i in Line:
+        print(i)
+
+def main():
+    print("Enter the FileName")
+    FileName = input()
+
+    Display(FileName)
+
+if __name__ == "__main__":
+    main()
